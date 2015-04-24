a72acc3b9a272505a5f9e27c9bf444609027e4c8
test <julien4.bunel@gmail.com>
2015-04-24 17:44:18 +0200

    envoie des exercices effectuer

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+puts "hello world"
+puts "hello again"
+puts "I like typing this"
+puts "this is fun"
+puts "yay! printing"
+puts "i'd much rather you not"
+puts "i said do not touch this"
---
+++ ex2.rb
@@ -0,0 +1,5 @@
+# A comment, this is so you can read your projet starter 
+# anything afeter the # is ignored by ruby
+puts "i could have code like this # and the comment  after is ingnored"
+# you can also use a comment to disable or comment out a piece of code
+puts "je pourrais avoir ce type de code."
